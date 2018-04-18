03237071061ef4fb89f18f0e5bbf93f271a42dc9
KKostya <kostya@aestheticintegration.com>
2018-04-18 08:23:10 +0000

    Update invriants

--- visualizer/make_jsons.sh
+++ visualizer/make_jsons.sh
@@ -1,2 +1,3 @@
-. mk_regions.sh ../decomposition_example.ml
-. mk_regions.sh ../six_swiss.ml
+. mk_json.sh ../decomposition_example.ml
+. mk_json.sh ../six_swiss.ml
+. mk_json.sh ../decomposition_tutorial1.ml
